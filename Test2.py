8abaec2e6e70f8bc25dbb15903c33b456d2eda4a
Phuwadon Decharam <phuwadon.dev@gmail.com>
2023-03-28 00:42:33 +0700

    commit

--- Test2.py
+++ Test2.py
@@ -15,7 +15,27 @@ print(products.describe())
 
 # 2 Select all data from the table orders and descriptive statistics for the orders table
 orders = pd.read_sql_query("SELECT * FROM orders", c)
-
 print(orders)
-
 print(orders.describe())
+
+# 3 Select all data from the table Order Details and descriptive statistics for the Order Details table
+order_details = pd.read_sql_query("SELECT * FROM `Order Details`", c)
+print(order_details)
+print(order_details.describe())
+
+# 4 Select all data from the table categories and descriptive statistics for the categories table
+categories = pd.read_sql_query("SELECT * FROM categories", c)
+print(categories)
+print(categories.describe())
+
+# 5 Select all data from the table customers and descriptive statistics for the customers table
+customers = pd.read_sql_query("SELECT * FROM customers", c)
+print(customers)
+print(customers.describe())
+
+# 6 Select o.OrderID ,o.CustomerID, o.EmployeeID, o.OrderDate, o.ShipAddress, o.CompanyName, c.ContactName, c.Phone, c.Country from orders o inner join customers c on order.CustomerID = customers.CustomerID
+order_customer = pd.read_sql_query("SELECT o.OrderID, o.CustomerID, o.EmployeeID, o.OrderDate, o.ShipAddress, c.CompanyName, c.ContactName, c.Phone, c.Country FROM orders o INNER JOIN customers c ON o.CustomerID = c.CustomerID;", c)
+print(order_customer)
+print(order_customer.describe())
+
+# Complete
